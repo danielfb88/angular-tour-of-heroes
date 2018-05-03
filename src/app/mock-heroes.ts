612fcf72b559846f2c1a5ae0e39ea030975c789f
Daniel Bonfim <daniel.bonfim@wexinc.com.br>
2018-05-03 14:17:08 -0300

    service em andamento

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Batman' },
+    { id: 12, name: 'Spider-man' },
+    { id: 13, name: 'IronMan' },
+    { id: 14, name: 'Dr. Esquisito' },
+    { id: 15, name: 'Aquateen' },
+    { id: 16, name: 'Pantera Negra' },
+    { id: 17, name: 'Sonic' },
+    { id: 18, name: 'Mario Bros' },
+    { id: 19, name: 'Maçã Verde' },
+    { id: 20, name: 'Goiaba' },
+]
