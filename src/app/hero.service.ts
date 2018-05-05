e3c352b2873899d6be202b3fee854d88873fbe1b
Daniel Bonfim <daniel.fb88@gmail.com>
2018-05-05 09:35:09 -0300

    Acessando servidor http://localhost:3000/api/heroes como /api/heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,16 +17,7 @@ const httpOptions = {
 
 @Injectable()
 export class HeroService {
-    private heroesUrl = 'http://localhost:3000/api/heroes'; // URL to web api
-
-    /** 
-     * Esta é a forma correta. 
-     * Ele tem que usar o arquivo proxy.conf.js. Pesquise.
-     * Por hora, está funcionando inserindo o host completo. Como está acima.
-     *
-     * Vou pro centro.
-     */
-    //private heroesUrl = '/api/heroes'; // URL to web api
+    private heroesUrl = '/api/heroes'; // URL to web api
 
     constructor(
         private messageService: MessageService,
